4ff537942471000f2d673b393c2ebd25b935bc74
sanjay pakale <sanjaypakale@gmail.com>
2020-04-19 07:09:24 +0530

    Added exception handling feature

--- src/main/java/com/sanjay/hibernateCrud/controller/EmployeeController.java
+++ src/main/java/com/sanjay/hibernateCrud/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.sanjay.hibernateCrud.controller;
 
+import com.sanjay.hibernateCrud.exception.ResourceNotFoundException;
 import com.sanjay.hibernateCrud.model.Employee;
 import com.sanjay.hibernateCrud.services.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,27 +17,31 @@ public class EmployeeController {
     private IEmployeeService employeeService;
 
     @GetMapping
-    public List<Employee> get(){
+    public List<Employee> get() {
         return employeeService.get();
     }
 
     @PostMapping
-    public void save(@RequestBody Employee employee){
+    public void save(@RequestBody Employee employee) {
         employeeService.save(employee);
     }
 
     @GetMapping("{id}")
-    public Employee getById(@PathVariable int id){
-        return employeeService.get(id);
+    public ResponseEntity<Employee> getById(@PathVariable int id) throws ResourceNotFoundException {
+        Employee employee = employeeService.get(id);
+        if (null == employee)
+            throw new ResourceNotFoundException("The employee not found for this id:: " + id);
+
+        return ResponseEntity.ok().body(employee);
     }
 
-    @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
-    public void deleteEmployee(@PathVariable int id){
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void deleteEmployee(@PathVariable int id) {
         employeeService.delete(id);
     }
 
     @PutMapping
-    public void updateEmployee(@RequestBody Employee employee){
+    public void updateEmployee(@RequestBody Employee employee) {
         employeeService.update(employee);
     }
 }
